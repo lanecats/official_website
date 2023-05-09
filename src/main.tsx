4d6c5eedb243c52abd518292ed8ea6bda867020c
Leancats <927497187@qq.com>
2023-05-09 10:00:24 +0800

    解决路径爆红问题

--- src/main.tsx
+++ src/main.tsx
@@ -2,17 +2,17 @@
  * @Author: Leancats 927497187@qq.com
  * @Date: 2023-04-24 10:04:08
  * @LastEditors: Leancats 927497187@qq.com
- * @LastEditTime: 2023-04-27 17:33:52
+ * @LastEditTime: 2023-05-09 09:51:51
  * @FilePath: /official_website/src/main.tsx
  * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
  */
-import React from 'react'
+// import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
 import './global.module.less'
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
-  <React.StrictMode>
+  // <React.StrictMode>
     <App />
-  </React.StrictMode>,
+  // </React.StrictMode>,
 )
--- vite.config.ts
+++ vite.config.ts
@@ -2,19 +2,20 @@
  * @Author: Leancats 927497187@qq.com
  * @Date: 2023-04-24 10:04:08
  * @LastEditors: Leancats 927497187@qq.com
- * @LastEditTime: 2023-04-24 11:28:06
+ * @LastEditTime: 2023-05-09 09:49:39
  * @FilePath: /official_website/vite.config.ts
  * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
  */
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import { resolve } from "path";
 
 export default defineConfig({
   plugins: [react()],
   resolve: {
     alias: {
-      '@': '/src',
-    }
+      "@": resolve(__dirname, "./src"),
+    },
   },
   server: {
     port: 8080,
