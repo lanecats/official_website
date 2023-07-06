3cad5f0bad4c1428afa3b73d43613e7f6fb9ebd7
Leancats <927497187@qq.com>
2023-07-06 15:38:03 +0800

    接口测试

--- src/services/api.ts
+++ src/services/api.ts
@@ -2,7 +2,7 @@
  * @Author: Leancats 927497187@qq.com
  * @Date: 2023-05-05 14:51:17
  * @LastEditors: Leancats 927497187@qq.com
- * @LastEditTime: 2023-05-05 15:19:36
+ * @LastEditTime: 2023-07-06 15:37:45
  * @FilePath: /webSite/src/services/index.ts
  * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
  */
@@ -10,7 +10,7 @@ import { request } from '../utils/request';
 import { UserListResponse, RequestConfig, User, ApiResponse } from '../utils/types';
 
 export const getUsers = (config?: RequestConfig): Promise<UserListResponse> => {
-    return request.get('/machineCitySum', config);
+    return request.get('/machineCitySums', config);
 };
 
 export const createUser = (data: Record<string, unknown>, config?: RequestConfig): Promise<ApiResponse<User>> => {
